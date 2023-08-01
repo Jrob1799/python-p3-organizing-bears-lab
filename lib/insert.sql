062d1bf4a97613aa177525ae29b777a59348c306
Jacob <jacobwroberts33@gmail.com>
2023-07-31 22:59:23 -0400

    update boolean values

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
-    ("Mr. chocolate", 5, "M", "Brown", "Friendly", true),
-    ('Rowdy', 3, 'M', 'Black', 'Energetic', true),
-    ('Tabitha', 2, 'F', 'Gray', 'Playful', true),
-    ('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', true),
-    ('Melissa', 4, 'F', 'White', 'Calm', true),
-    ('Grinch', 1, 'M', 'Green', 'Mischievous', true),
-    ('Wendy', 6, 'F', 'Brown', 'Shy', true),
-    (NULL, NULL, NULL, NULL, NULL, false);
+    ("Mr. chocolate", 5, "M", "Brown", "Friendly", 1),
+    ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+    ('Tabitha', 2, 'F', 'Gray', 'Playful', 1),
+    ('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', 1),
+    ('Melissa', 4, 'F', 'White', 'Calm', 1),
+    ('Grinch', 1, 'M', 'Green', 'Mischievous', 1),
+    ('Wendy', 6, 'F', 'Brown', 'Shy', 1),
+    (NULL, NULL, NULL, NULL, NULL, 0);
