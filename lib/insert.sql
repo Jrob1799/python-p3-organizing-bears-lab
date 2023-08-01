691aace07dd897bf912499113273bb1bbb2c66f1
Jacob <jacobwroberts33@gmail.com>
2023-07-31 22:44:46 -0400

    pass all tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ("Mr. chocolate", 5, "M", "Brown", "Friendly", true),
+    ('Rowdy', 3, 'M', 'Black', 'Energetic', true),
+    ('Tabitha', 2, 'F', 'Gray', 'Playful', true),
+    ('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', true),
+    ('Melissa', 4, 'F', 'White', 'Calm', true),
+    ('Grinch', 1, 'M', 'Green', 'Mischievous', true),
+    ('Wendy', 6, 'F', 'Brown', 'Shy', true),
+    (NULL, NULL, NULL, NULL, NULL, false);
